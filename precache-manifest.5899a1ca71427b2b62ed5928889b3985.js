876db49fad130ef396a37d6e55b646f742d4b67b
TanimaRanjan <tanimaranjan@gmail.com>
2019-11-06 17:20:33 -0500

    Updates

---
+++ precache-manifest.5899a1ca71427b2b62ed5928889b3985.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c9c357d55bd86d3dd69dfdd63fc4b8b2",
+    "url": "/monsters-inc/index.html"
+  },
+  {
+    "revision": "b904c92211eec3bc932d",
+    "url": "/monsters-inc/static/css/main.ab59af33.chunk.css"
+  },
+  {
+    "revision": "6eaf076458442a2de4f7",
+    "url": "/monsters-inc/static/js/2.25c7b785.chunk.js"
+  },
+  {
+    "revision": "b904c92211eec3bc932d",
+    "url": "/monsters-inc/static/js/main.8a48a51a.chunk.js"
+  },
+  {
+    "revision": "28bac05eb0d77732982e",
+    "url": "/monsters-inc/static/js/runtime-main.b9c9805c.js"
+  }
+]);
