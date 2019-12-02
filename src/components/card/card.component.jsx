9a81e3fe5adbb3db981ac1197341c0e90995f1c6
TanimaRanjan <tanimaranjan@gmail.com>
2019-12-01 21:50:04 -0500

    Card list

--- src/components/card/card.component.jsx
+++ src/components/card/card.component.jsx
@@ -2,13 +2,13 @@ import React from 'react'
 
 import './card.css'
 
-export const Card = (props) => {
-    console.log(props.monster)
+export const Card = ({monster}) => {
+    console.log(monster)
     return (
         <div className="card">
-        <img src={`https://robohash.org/${props.monster.id}?set=set2&size=180x180`} alt="Monster"></img>
-        <h3>{props.monster.name}</h3>
-        <p>{props.monster.email}</p>
+        <img src={`https://robohash.org/${monster.id}?set=set2&size=180x180`} alt={monster.name}></img>
+        <h3>{monster.name}</h3>
+        <p>{monster.email}</p>
         </div>
     )
 }
